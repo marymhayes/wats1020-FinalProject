e88371b46ea300c9ad7df3452e32328f2a1f666c
hayesm3 <hayesm3@seattleu.edu>
2016-06-05 13:38:29 -0400

    completed final project

--- js/main.js
+++ js/main.js
@@ -1,4 +1,36 @@
 $(document).on('ready', function(){
+  
+// Added on click event listener for That Thing email address submit button
+// and that hides email form and shows thank you note.
+  $( '#thatthingform .btn' ).on( 'click', function() {
+    console.log( "email address submitted" );
+    
+    $('#thatthingform').hide();
+    
+    $('#thanks').show('fast');
+});
+  
+  // Added variable and function for Twitter Post Fetcher created by 
+  // Jason Mayes https://github.com/jasonmayes/Twitter-Post-Fetcher
+  // Limited shown tweets to single most recent. Links enabled, hid
+  // retweet, reply options.
+  var twitterFeed = {
+  "id": '738830171759738880',
+  "domId": 'twitterFeed',
+  "maxTweets": 1,
+  "enableLinks": true,
+  "showPermalinks": false,
+  "showInteraction": false
+};
+twitterFetcher.fetch(twitterFeed);
+
+// added Scroll reveal...fixed and made work thanks to help from Mary Pringle!
+  window.sr = ScrollReveal();
+  sr.reveal('#introimage');
+  sr.reveal('#bio');
+  sr.reveal('#links');
+  sr.reveal('#videoheader');
+  sr.reveal('#thatthing');
 
 
 //Added code to optimize experience depending on viewport size found at https://andylangton.co.uk/blog/development/get-viewportwindow-size-width-and-height-javascript.
@@ -27,15 +59,4 @@ $(document).on('ready', function(){
    }
    return [viewPortWidth, viewPortHeight];
   }
-
 });
-
-// added Scroll reveal
-
-/* ******* Need to work through *******
-window.sr = ScrollReveal();
-sr.reveal('#navigation');
-sr.reveal('#aboutimage');
-sr.reveal('#imageheader');
-sr.reveal('#videoheader');
-*/
